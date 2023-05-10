733f17c12d6914fdcebcfbd9f30a31f53857680d
Bernhard Fritz <bernhard.e.fritz@gmail.com>
2023-05-11 00:03:43 +0200

    Initialize project using cookiecutter-p8g

---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import topLevelAwait from 'vite-plugin-top-level-await';
+
+export default defineConfig(({ command }) => ({
+  plugins: [
+    topLevelAwait(),
+    ...[
+      command === 'serve'
+        ? createHtmlPlugin({
+            inject: {
+              tags: [
+                {
+                  injectTo: 'head-prepend',
+                  tag: 'script',
+                  children: `// REGISTER ERROR OVERLAY
+const showErrorOverlay = (err) => {
+// must be within function call because that's when the element is defined for sure.
+const ErrorOverlay = customElements.get('vite-error-overlay');
+// don't open outside vite environment
+if (!ErrorOverlay) {
+return;
+}
+console.log(err);
+const overlay = new ErrorOverlay(err);
+document.body.appendChild(overlay);
+};
+
+window.addEventListener('error', showErrorOverlay);
+window.addEventListener('unhandledrejection', ({ reason }) =>
+showErrorOverlay(reason),
+);
+`,
+                },
+              ],
+            },
+          })
+        : [],
+    ],
+  ],
+}));
